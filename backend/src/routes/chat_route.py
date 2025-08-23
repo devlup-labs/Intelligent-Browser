f9ace8470c48c4a3ef80e3e486ee62f8f77d7fd6
Akhil Dhyani <akhildhyani420@gmail.com>
2025-08-24 02:24:43 +0530

    fic merge issues

--- backend/src/routes/chat_route.py
+++ backend/src/routes/chat_route.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter,Depends,HTTPException,status,WebSocket,WebSocketDisconnect
-
 from sqlalchemy.orm import Session 
 from src.schema import schema
 from src.database.database import SessionLocal
@@ -8,15 +7,14 @@ from src.schema import schema
 from fastapi.responses import JSONResponse
 from src.middleware.get_current_user import getCurrentUser
 from src.agents.main import run
-# Update the import path below if connection_manager.py is in a different location
 from src.utils.connection_manager import manager
-
 import nest_asyncio
-
 
 nest_asyncio.apply()
 
 chatRouter=APIRouter()
+
+
 
 def get_db():
     db=SessionLocal()
@@ -50,5 +48,3 @@ async def websocket_endpoint(websocket:WebSocket):
 #     result = await run(user_request)
 #     output=result
 #     return JSONResponse(content=result)
-
-
